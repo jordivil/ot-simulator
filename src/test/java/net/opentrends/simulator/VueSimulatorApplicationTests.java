2c0b210da261557198a198d2e81604d143715e69
Jordi Villar <jordi.villar.bcn@gmail.com>
2021-03-02 10:54:20 +0100

    commit inicial

---
+++ src/test/java/net/opentrends/simulator/VueSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.opentrends.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
